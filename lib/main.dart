0f99ae4839b5ce06d948e05ec2843d3557687cce
ogusam <ogusam32@gmail.com>
2023-06-16 12:37:44 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(/*const*/ MaterialApp(
+    home: Scaffold(
+      //backgroundColor: Colors.purple,
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color.fromARGB(255,26,2,80),
+                     Color.fromARGB(255, 45, 7, 98),
+            ]
+           )
+        ),
+        child: const Center(
+          child: Text('Hello world'),
+        ),
+      ),
+  ),
+  ),
+  );
+}
